b4e73dbb62eb2eb0220b4155ca126c73f1379287
arhambutt11 <72912177+arhambutt11@users.noreply.github.com>
2021-02-26 15:27:07 -0800

    merging

---
+++ webpage.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/webpage.css" rel="stylesheet">
+
+
+</head>
+<body>
+
+
+    <header>
+        <div class="headerback">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-2 col-xs-12">
+                        <div class="logoArea">
+                            <a href="#"><img src="images/wlogo.png" class="img-responsive"></a>
+                        </div>
+                    </div>
+                    <div class="col-sm-10 col-xs-12">
+                        <div class="row mobileNav">
+                            <div class="col-xs-4 visible-xs xs-marker">
+                                <!-- <a href="https://goo.gl/maps/iE4esX3uT4hkjbuE9" target="_blank" title="Google Map"><i
+                    class="fa fa-map-marker "></i>Find</a> -->
+                            </div>
+                            <div class="col-xs-4 visible-xs xs-phone">
+                                <!-- <a href="tel:+92-21-38897770" target="_blank" title="Contact Number"><i
+                    class="fa fa-phone "></i>Call</a> -->
+                            </div>
+                            <div class="col-xs-4 visible-xs">
+                                <button data-target=".navbar-collapse" data-toggle="collapse" id="mnav-button"
+                                    class="navbar-toggle fa fa-bars fa-2x collapsed" type="button">
+                                </button>
+                            </div>
+                        </div>
+                        <nav>
+                            <div class="jump">
+                                <div class="navbar-collapse nav-collapse collapse">
+                                    <ul class="nav navbar-nav hove">
+                                        <li id="11">
+                                            <a href="#"><span class="headpad">HOME</span> </a>
+                                        </li>
+                                        <li id="4">
+                                            <a href="#"><span class="headpad">ABOUT</span> </a>
+                                        </li>
+                                        <li id="5">
+                                            <a href="#"><span class="headpad">CONTACT</span> </a>
+                                        </li>
+                                        <li id="6">
+                                            <a href="#"><span class="headpad">LOGIN</span> </a>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="mainbanner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-5">
+                        <div class="mainbannerdata">
+                            <img src="images/fgf.png" class="img-responsive">
+                            <h2>THE ULTIMATE AMAZON FLEX BOT</h2>
+                            <p>We show you how to create your account and
+                                configure Amazon Flex Bot.app in 5 minutes.</p>
+                            <p>After that, wait and see! We get you some
+                                deliveries to work on with Amazon Flex Bot!</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-7">
+                        <div class="mainbannerimg">
+                            <img src="images/bg.png" class="img-responsive">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="threebox">
+            <div class="container">
+                <div class="row">
+                    <div class="threeboxdata">
+                        <h2>The Most Popular app to <span class="blue">Amazon Flex Bot</span></h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="bluebox">
+                            <img src="images/icon-p.png" class="img-responsive">
+                            <h4>+5,000 Satisfied Customers</h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="bluebox">
+                            <img src="images/icon-l.png" class="img-responsive">
+                            <h4>+60,000 Blocks Grabbed</h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="bluebox">
+                            <img src="images/icon-w.png" class="img-responsive">
+                            <h4>+$2,000,000 Payouts Earned</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="featurearea">
+            <div class="container">
+                <div class="row">
+                    <div class="featureareadata">
+                        <h2>Check out our cool features!</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="featureareabox">
+                            <div class="featureareaboximg">
+                                <img src="images/setting.png" class="img-responsive">
+                            </div>
+                            <div class="featureareaboxdata">
+                                <h4>Automatic Search</h4>
+                                <p>Configure your work preferences, activate the
+                                    search, get your block!
+                                    You don't need to catch any blocks by yourself.
+                                    We do it for you.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="featureareabox">
+                            <div class="featureareaboximg">
+                                <img src="images/clock.png" class="img-responsive">
+                            </div>
+                            <div class="featureareaboxdata">
+                                <h4>Spare Your Time</h4>
+                                <p>With the Amazon Flex Bot app, no need to
+                                    manually search anymore. Your time is
+                                    precious, stop losing it while waiting for work
+                                    to come!
+                                </p>
+                            </div>
+                        </div>
+                        <div class="featureareabox">
+                            <div class="featureareaboximg">
+                                <img src="images/lock.png" class="img-responsive">
+                            </div>
+                            <div class="featureareaboxdata">
+                                <h4>It is safe</h4>
+                                <p>Create a unique account for Amazon Flex. No need to give your
+                                    Amazon Flex credentials away! We will swipe
+                                    blocks as if it was you. and Amazon Flex won't see
+                                    any difference!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="featureareabox">
+                            <div class="featureareaboximg">
+                                <img src="images/map.png" class="img-responsive">
+                            </div>
+                            <div class="featureareaboxdata">
+                                <h4>International Support</h4>
+                                <p>The Amazon Flex Bot app is supported in the
+                                    United States, in Japan, Spain, Germany and in
+                                    the United Kingdom. Grab more blocks
+                                    around the world!
+                                </p>
+                            </div>
+                        </div>
+                        <div class="featureareabox">
+                            <div class="featureareaboximg">
+                                <img src="images/rocket.png" class="img-responsive">
+                            </div>
+                            <div class="featureareaboxdata">
+                                <h4>Earn more</h4>
+                                <p>Take advantage of Amazon Flex with our app.
+                                    We let you dedicate yourself to what the most:
+                                    deliver more to earn more
+                                </p>
+                            </div>
+                        </div>
+                        <div class="featureareabox">
+                            <div class="featureareaboximg">
+                                <img src="images/smile.png" class="img-responsive">
+                            </div>
+                            <div class="featureareaboxdata">
+                                <h4>Feel secure</h4>
+                                <p>Being on the phone while driving is no good
+                                    and we all know it! You can now stay focused
+                                    on your driving, you will be notified when we
+                                    grab some blocks for you.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="bigpicture">
+                            <img src="images/screen.jpg" class="img-responsive">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="subscriptionarea">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="dollardatra">
+                            <h2>$25</h2>
+                            <h3>Monthly subscription</h3>
+                            <h4>Weekly Payments $25 5 Days Free Trail</h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="dollarimg">
+                            <img src="images/payout.png" class="img-responsive">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="contactarea">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="contactusdata">
+                            <h2>CONTACT US</h2>
+                            <p>We’re happy to answer any questions you have or provide you with an estimate.
+                                just send a message in the form below with any questions you may have.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row formarea">
+                    <div class="col-sm-12">
+                        <div class="form-group farmform">
+                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                aria-describedby="emailHelp" placeholder="YOUR NAME (required)">
+                        </div>
+                        <div class="form-group farmform">                            
+                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                aria-describedby="emailHelp" placeholder="YOUR EMAIL (required)">
+                        </div>
+                        <div class="form-group farmform">                            
+                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                aria-describedby="emailHelp" placeholder="Enter Subject">
+                        </div>
+                        <div class="form-group farmform">
+                            <textarea class="form-control farmformtext" id="exampleFormControlTextarea1" rows="6"
+                                placeholder="Enter Message"></textarea>
+                        </div>
+                        <div class="formclick">
+                            <button type="button" class="btn btn-primary send">SEND MESSAGE</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="bluebackarea">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm=12">
+                        <div class="bluebackareadata">
+                            <p>Copyright © 2021 Amazon Flex Bot | All Rights Reserved | Designed by <a
+                                    href="https://hatinco.com/">HATINC.</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+</body>
+</html>
